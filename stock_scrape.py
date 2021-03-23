ce40e74470f1dd33e52cbd1a72c26607165b16ec
sunnywing <43187851+sunnywing@users.noreply.github.com>
2021-03-23 03:38:43 -0500

    Both websites return expected data appropriately

--- stock_scrape.py
+++ stock_scrape.py
@@ -1,5 +1,6 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 opts = Options()
 opts.headless = True
 browser = Chrome(options=opts)
@@ -9,29 +10,43 @@ with open('C:/Users/pluhm/Documents/graphics_links.txt', 'r') as f:
         link = line.strip()
         browser.get(link)
 
-        if "bestbuy" in link: #bestbuy coding needs worked on to get the desired result
+        if "bestbuy" in link:
             web_class = (
-                'sku-title', #title
-                'priceView-customer-price', #price
-                'fulfillment-add-to-cart-button' #status
+                "sku-title", #title
+                "div.priceView-hero-price.priceView-customer-price", #price
+                "div.fulfillment-add-to-cart-button" #status
             )
+            
+            title = browser.find_element_by_class_name(web_class[0]).text
+            #price = browser.find_element_by_css_selector(web_class[1]).text
+            html = browser.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            price = soup.find("div", class_="priceView-price-match-guarantee").next_sibling.find('span').text
+
+            try:
+                #status = browser.find_element_by_css_selector(web_class[2]).text
+                status = soup.find("div", class_="fulfillment-add-to-cart-button").find('button').text
+            except:
+                status = "OUT OF STOCK"
+            print("********" + title + "********")
+            print(price)
+            print(status)
         
         if "newegg" in link:
             web_class = (
-                'product-title', #title
-                'product-price', #price
-                'product-buy' #status
+                "product-title", #title
+                "li.price-current", #price
+                "button.btn.btn-primary.btn-wide" #status
             )
-        
-        title = browser.find_element_by_class_name(web_class[0]).text
-        price = browser.find_element_by_class_name(web_class[1]).text
-        status = browser.find_element_by_class_name(web_class[2]).text
-        
-        print("********" + title + "********")
-        print(price)
-        if status == '':
-            print("OUT OF STOCK")
-        else:
+            title = browser.find_element_by_class_name(web_class[0]).text
+            price = browser.find_element_by_css_selector(web_class[1]).text
+            try:
+                status = browser.find_element_by_css_selector(web_class[2]).text
+                
+            except:
+                status = "OUT OF STOCK"
+            print("********" + title + "********")
+            print(price)
             print(status)
-
+        
 browser.quit()
